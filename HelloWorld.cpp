db40acc8f74107cdec94e5a41f0b5e30f66a4cef
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-24 18:34:00 +0300

    first commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "I like pizza!" << '\n';
+
+    //integer (whole number)
+    int age = 21;
+    int year = 2023;
+    int days = 7.5;
+
+    //double (number including decimal)
+
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    //single character
+
+    char grade = 'A';
+    char initial = 'B';
+
+    //boolean (True or False)
+    bool student = true;
+    bool power = false;
+    bool ForSale = true;
+
+    //string (object that represents a sequence of text)
+    std::string name = "Tamir";
+    std::string Last_name = "Rozenfeld";
+    std::string Food = "Pizza";
+    
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old" ;
+
+    
+    
+
+    return 0;
+}
