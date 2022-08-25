e2091b82be1cffdcd7c7725379f477b8e494968f
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-25 15:42:41 +0300

    Added

---
+++ NameSpace.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+
+int main(){
+    using std::cout;
+    using std::string;
+
+    string name = "Tamir";
+
+    cout << "Hello " << name;
+    
+
+    
+
+    return 0;
+}
+
