1b09e9f29bd43489949846d4475288e6cacebf40
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-25 20:22:31 +0300

    ArithmeticOP

---
+++ Arithmetic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+    int students = 20;
+    //double students = 20;
+    //int students = 20;
+    //students+=2;
+    //students++;
+    //students-=2;
+    //students--;
+    //students*=2;
+    int remainder = students % 3;
+
+    //students/=3;
+    //std::cout << students;
+    std::cout << remainder;
+    
+
+    return 0;
+}
