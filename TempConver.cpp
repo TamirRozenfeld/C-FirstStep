e8c01be86dabda1a3e7998e092b3a5c16deaba28
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-26 20:47:29 +0300

    Temperature conversion

---
+++ TempConver.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+        
+    double temp;
+    char unit;
+
+    std::cout << "********** Temperature Conversion **********\n";
+    std::cout << "F = Fahrenheit\n";
+    std::cout << "C = Celsius\n";
+    std::cout << "What unit would u like to convert to: \n";
+    std::cin >> unit;
+    
+
+    if(unit == 'F' || unit == 'f'){
+        std::cout << "Enter the temperature in Celsius: \n";
+        std::cin >> temp;
+
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "Temperature is: " << temp << " F\n";
+        
+    }
+        else if(unit == 'C' || unit == 'c'){
+            std::cout << "Enter the temperature in Fahrenheit: \n";
+            std::cin >> temp;
+            temp = (temp - 32)/1.8;
+            std::cout << "Temperature is: " << temp << " C\n";
+        }
+        else{
+            std::cout << "Please enter in only C or F \n";
+        }
+        std::cout << "********** Temperature **********\n";
+    return 0;
+}
