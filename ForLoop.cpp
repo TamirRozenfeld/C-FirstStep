4de782e3e469a8655031d26c0c308df981873897
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-27 13:49:30 +0300

    ForLoop

---
+++ ForLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    for(int i = 10; i >= 0; i-=1){
+
+        std::cout << i << '\n';
+        
+    }
+        
+    std::cout << "HAPPY NEW YEAR!\n";
+
+    return 0;
+}
