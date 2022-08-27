c40db700e82f669f635ace4fb8e0c83e6a438606
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-27 08:21:56 +0300

    UpdateWhileLoop

--- While.cpp
+++ While.cpp
@@ -3,11 +3,26 @@
 
 int main(){
 
-    std::string name;
-    while(name.empty()){
-        std::cout << "Enter your name: ";
-        std::getline(std::cin, name);
-    }
-       std::cout << "Hello " << name;
+    //std::string name;
+    //while(name.empty()){
+    //    std::cout << "Enter your name: ";
+    //    std::getline(std::cin, name);
+    //}
+    //   std::cout << "Hello " << name;
+
+    int number;
+
+        
+
+    do{
+
+        std::cout << "Enter a positive number #: ";
+        std::cin >> number;
+    
+    }while(number < 0);
+    
+
+    std::cout << "The # is: " << number;
+
     return 0;
 }
