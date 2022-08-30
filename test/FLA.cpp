3dee5012e82db0c543cd6eab1a284e6b28e061a5
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-30 10:52:39 +0300

    TAMIRRR

---
+++ test/FLA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+    
+    //std::string students[] = {"Eran", "David", "Tamir"};
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+
+    for(int i = 0; i < sizeof(grades)/sizeof(char); i++){
+        std::cout << grades[i] << '\n';
+
+    }
+    
+    return 0;
+}
