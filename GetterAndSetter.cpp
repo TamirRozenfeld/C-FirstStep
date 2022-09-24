474f05c7010d47064f6933fdf233a685cc301aad
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-09-24 08:43:28 +0300

    Getter & Setter

---
+++ GetterAndSetter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class Stove{
+    private:
+        int temperature = 0;
+    public:
+    Stove(int temperature){
+        setTemperature(temperature);
+    }
+        int getTemperature(){
+            return temperature;
+        }
+    void setTemperature(int temperature){
+        this->temperature = temperature;
+            if(temperature < 0){
+        this->temperature = 0;
+    }
+             else if(temperature >= 10){
+        this->temperature = 10;
+    }
+
+            else{
+       this->temperature = temperature; 
+    }
+
+    }
+};
+  
+int main()
+{   
+    Stove stove(0); 
+
+    stove.setTemperature(5);
+
+
+    std::cout << "The temperature setting is: " << stove.getTemperature() << '\n';
+
+    return 0;
+}
